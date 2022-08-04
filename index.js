ec865f5310ddc4f98ac91b135705d681ec85c1fa
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-08-04 15:20:58 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+let employee = {
+    name: "Chris",
+    streetAddress: 239
+}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = {...employee};
+    newObj[key] = value;
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee};
+    newEmployee[key] = 'name';
+    delete newEmployee.name;
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    employee[key] = 'name'
+    delete employee.name;
+    return employee;
+}
